17e86077cdbb6d3b5e15b44447fc5fa010f02dc5
C. E. Ball <ceball@users.sourceforge.net>
2012-07-17 22:50:18 +0100

    Cleaned up packaging.

---
+++ README.txt
@@ -0,0 +1,20 @@
+`Param`_ is a library providing Parameters: Python attributes extended
+to have features such as type and range checking, dynamically
+generated values, documentation strings, default values, etc., each of
+which is inherited from parent classes if not specified in a subclass.
+
+Please see http://ioam.github.com/param/ for more information.
+
+Installation
+============
+
+Param has no dependencies outside the standard library, and can be
+installed via ``easy_install param`` or ``pip install
+param``. Alternatively, Windows users can download and run the exe;
+others can download and unpack the source tar.gz, and then install
+with a command like ``python setup.py install`` (e.g. ``sudo python
+setup.py install`` for a site-wide installation, or ``python setup.py
+install --user`` to install into ``~/.local``).
+
+.. _Param:
+   http://ioam.github.com/param/
--- setup.py
+++ setup.py
@@ -10,30 +10,7 @@ setup_args.update(dict(
     name='param',
     version='0.01',
     description='Declarative Python programming using Parameters.',
-
-    long_description="""
-`Param`_ is a library providing Parameters, a special type of Python
-attribute extended to have features such as type and range checking,
-dynamically generated values, documentation strings, default values,
-etc., each of which is inherited from parent classes if not specified
-in a subclass.
-
-Please see http://ioam.github.com/param/ for more information.
-
-Installation
-============
-
-Param has no dependencies outside the standard library, and can be
-installed via ``easy_install param`` or ``pip install
-param``. Alternatively, you can download and unpack the archive below,
-and then install with a command like ``python setup.py install``
-(e.g. ``sudo python setup.py install`` for a site-wide installation,
-or ``python setup.py install --user`` to install into ``~/.local``).
-
-.. _Param:
-   http://ioam.github.com/param/
-""",
-
+    long_description=open('README.txt').read(),
     author= "IOAM",
     author_email= "developers@topographica.org",
     maintainer= "IOAM",

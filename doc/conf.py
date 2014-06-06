5861873c302103004d1e2ce2eae650849bd844b2
philippjfr <P.Rudiger@ed.ac.uk>
2014-06-06 13:28:17 +0100

    Fixed sys.path issues in Sphinx conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from builder.shared_conf import *  # pyflakes:ignore (API import)
 
 paths = ['.', '..']

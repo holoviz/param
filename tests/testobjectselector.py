6c4570994ff233d12dc0b0edb98ddb68f3e70691
jlstevens <jlstevens@ed.ac.uk>
2014-07-02 01:42:52 +0100

    Converted Topographica testEnumerationParameter doctest to unit tests

---
+++ tests/testobjectselector.py
@@ -0,0 +1,102 @@
+"""
+Unit test for object selector parameters.
+
+Originally implemented as doctests in Topographica in the file
+testEnumerationParameter.txt
+"""
+
+import unittest
+import param
+
+
+class TestObjectSelectorParameters(unittest.TestCase):
+
+    def setUp(self):
+
+        class P(param.Parameterized):
+            e = param.ObjectSelector(default=5,objects=[5,6,7])
+            f = param.ObjectSelector(default=10)
+            h = param.ObjectSelector(default=None)
+            g = param.ObjectSelector(default=None,objects=[7,8])
+            i = param.ObjectSelector(default=7,objects=[9],check_on_set=False)
+
+        self.P = P
+
+    def test_set_object_constructor(self):
+        p = self.P(e=6)
+        self.assertEqual(p.e, 6)
+
+    def test_set_object_outside_bounds(self):
+        p = self.P(e=6)
+        try:
+            p.e = 9
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("Object set outside range.")
+
+    def test_set_object_setattr(self):
+        p = self.P(e=6)
+        p.f = 9
+        self.assertEqual(p.f, 9)
+        p.g = 7
+        self.assertEqual(p.g, 7)
+        p.i = 12
+        self.assertEqual(p.i, 12)
+
+
+    def test_set_object_not_None(self):
+        p = self.P(e=6)
+        p.g = 7
+        try:
+            p.g = None
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("Object set outside range.")
+
+    def test_set_object_setattr_post_error(self):
+        p = self.P(e=6)
+        p.f = 9
+        self.assertEqual(p.f, 9)
+        p.g = 7
+        try:
+            p.g = None
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("Object set outside range.")
+
+        self.assertEqual(p.g, 7)
+        p.i = 12
+        self.assertEqual(p.i, 12)
+
+    def test_initialization_out_of_bounds(self):
+        try:
+            class Q(param.Parameterized):
+                q = param.ObjectSelector(5,objects=[4])
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("ObjectSelector created outside range.")
+
+
+    def test_initialization_no_bounds(self):
+        try:
+            class Q(param.Parameterized):
+                q = param.ObjectSelector(5,objects=10)
+        except TypeError:
+            pass
+        else:
+            raise AssertionError("ObjectSelector created without range.")
+
+
+if __name__ == "__main__":
+    import nose
+    nose.runmodule()
+
+
+
+
+
+

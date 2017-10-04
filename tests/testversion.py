64c8f7cf371a5e847dc5ead619e0581877db6785
Jean-Luc Stevens <jlstevens@ed.ac.uk>
2017-10-05 00:50:29 +0100

    Added dev argument to param.Version to support dev versions (#200)
    Accepts github tags like "v1.9.0dev3" or "v1.9.0.dev3", with the reported version string always following PEP440 ("1.9.0.dev3" in this case), and with comparisons correctly sorting dev releases before full releases.

--- tests/testversion.py
+++ tests/testversion.py
@@ -23,6 +23,10 @@ class TestVersion(unittest.TestCase):
         v101 = Version(release=(1,0,1), commit_count=10, commit='aaaaaaa')
         self.assertEqual(repr(v101), '1.0.1-10-gaaaaaaa')
 
+    def test_repr_v101dev4_10_commits(self):
+        v101 = Version(release=(1,0,1), commit_count=10, commit='aaaaaaa', dev=4)
+        self.assertEqual(repr(v101), '1.0.1.dev4-10-gaaaaaaa')
+
     def test_version_init_v101(self):
         Version(release=(1,0,1))
 
@@ -33,6 +37,10 @@ class TestVersion(unittest.TestCase):
     def test_version_str_v1(self):
         v1 = Version(release=(1,0))
         self.assertEqual(str(v1), '1.0')
+
+    def test_version_str_v1dev3(self):
+        v1 = Version(release=(1,0),dev=3)
+        self.assertEqual(str(v1), '1.0.dev3')
 
     def test_version_v1_dirty(self):
         v1 = Version(release=(1,0))
@@ -63,6 +71,34 @@ class TestVersion(unittest.TestCase):
         v1 = Version(release=(1,0), commit='shortSHA')
         self.assertEqual(v1.commit, 'shortSHA')
 
+    #===========================================#
+    #  Update from VCS (currently git describe) #
+    #===========================================#
+
+    def test_version_simple_git_describe(self):
+        v105 = Version(release=(1,0,5))
+        v105._update_from_vcs('v1.0.5-42-gabcdefgh')
+        self.assertEqual(v105.release, (1,0,5))
+        self.assertEqual(v105.commit_count, 42)
+        self.assertEqual(v105.dev, None)
+        self.assertEqual(v105.commit, 'abcdefgh')
+
+    def test_version_dev_pep440_git_describe(self):
+        v105 = Version(release=(1,0,5), dev=4)
+        v105._update_from_vcs('v1.0.6.dev4-42-gabcdefgh')
+        self.assertEqual(v105.release, (1,0,6))
+        self.assertEqual(v105.commit_count, 42)
+        self.assertEqual(v105.dev, 4)
+        self.assertEqual(v105.commit, 'abcdefgh')
+
+    def test_version_dev_wo_pep440_git_describe(self):
+        v105 = Version(release=(1,0,5), dev=4)
+        v105._update_from_vcs('v1.0.6dev4-42-gabcdefgh')
+        self.assertEqual(v105.release, (1,0,6))
+        self.assertEqual(v105.commit_count, 42)
+        self.assertEqual(v105.dev, 4)
+        self.assertEqual(v105.commit, 'abcdefgh')
+
     #===========================#
     #  Comparisons and equality #
     #===========================#
@@ -72,10 +108,25 @@ class TestVersion(unittest.TestCase):
         v101 = Version(release=(1,0,1))
         self.assertEqual((v1 < v101), True)
 
+    def test_version_less_than_dev(self):
+        v101 = Version(release=(1,0,1))
+        v101dev2 = Version(release=(1,0,1), dev=2)
+        self.assertEqual((v101dev2 < v101), True)
+
+    def test_version_less_than_dev_number(self):
+        v101dev1 = Version(release=(1,0,1), dev=1)
+        v101dev2 = Version(release=(1,0,1), dev=2)
+        self.assertEqual((v101dev1 < v101dev2), True)
+
     def test_version_greater_than(self):
         v1 = Version(release=(1,0))
         v101 = Version(release=(1,0,1))
         self.assertEqual((v1 > v101), False)
+
+    def test_version_greater_than_dev(self):
+        v101 = Version(release=(1,0,1))
+        v102dev3 = Version(release=(1,0,2), dev=3)
+        self.assertEqual((v102dev3 > v101), True)
 
     def test_version_eq_v1(self):
         v1 = Version(release=(1,0))
@@ -84,6 +135,15 @@ class TestVersion(unittest.TestCase):
     def test_version_eq_v101(self):
         v101 = Version(release=(1,0,1))
         self.assertEqual(v101==v101, True)
+
+    def test_version_eq_v101_dev(self):
+        v101dev3 = Version(release=(1,0,1), dev=3)
+        self.assertEqual(v101dev3==v101dev3, True)
+
+    def test_version_neq_v101_dev(self):
+        v101dev2 = Version(release=(1,0,1), dev=2)
+        v101dev3 = Version(release=(1,0,1), dev=3)
+        self.assertEqual(v101dev2!=v101dev3, True)
 
     def test_version_neq(self):
         v1 = Version(release=(1,0))

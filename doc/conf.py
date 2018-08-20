a4e617d89caea9df83ab3ac0e14b48222c228769
Chris B <ceball@users.noreply.github.com>
2018-08-20 11:00:38 +0100

    Use nbsite to generate documentation website (#247)
    Note about a side effect of this change: param conda packages previously appeared on pyviz/label/dev for master, but will now only appear for explicitly dev tags (in line with pyviz convention).

--- doc/conf.py
+++ doc/conf.py
@@ -1,73 +1,39 @@
 # -*- coding: utf-8 -*-
 
-import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from nbsite.shared_conf import *
 
-from builder.shared_conf import *  # pyflakes:ignore (API import)
-
-paths = ['.', '..']
-add_paths(paths)
-
-# General information about the project.
 project = u'Param'
-copyright = u'\u00a9 2003-2018, IOAM'
-ioam_project = 'param'
-from param import __version__
+authors = u'PyViz authors'
+copyright = u'\u00a9 2005-2018, ' + authors
+description = 'Declarative Python programming using Parameters.'
 
+import param
+version = release = param.__version__
 
-# The version info for the project being documented, defining |version|
-# and |release| and used in various other places throughout the built
-# documents.  Assumes __version__ is a param.version.Version object.
-#
-# The short X.Y.Z version.
-version = __version__.abbrev()
+html_static_path += ['_static']
+html_theme = 'sphinx_ioam_theme'
+html_theme_options = {
+    'logo':'logo.png',
+    'favicon':'favicon.ico',
+#    'css':'site.css'
+}
 
-# The full version, including alpha/beta/rc/dev tags.
-release = __version__.abbrev(dev_suffix="-dev")
+_NAV =  (
+    ('API', 'Reference_Manual/param'),
+    ('About', 'About'),
+)
 
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-exclude_patterns = ['_build']
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', 'builder/_shared_static']
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = project + 'doc'
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-  ('index', project+'.tex', project + ' Documentation',
-   u'IOAM', 'manual'),
-]
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    ('index', ioam_project, project + ' Documentation',
-     [u'IOAM'], 1)
-]
-
-# If true, show URL addresses after external links.
-#man_show_urls = False
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-  ('index', project, project + ' Documentation',
-   u'IOAM', project, 'One line description of project.',
-   'Miscellaneous'),
-]
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+html_context.update({
+    'PROJECT': project,
+    'DESCRIPTION': description,
+    'AUTHOR': authors,
+    # canonical URL (for search engines); can ignore for local builds
+    'WEBSITE_SERVER': 'https://param.pyviz.org',
+    'VERSION': version,
+    'NAV': _NAV,
+    'LINKS': _NAV,
+    'SOCIAL': (
+        ('Gitter', '//gitter.im/ioam/holoviews'),
+        ('Github', '//github.com/ioam/param'),
+    )
+})

efc4051df7eefbfcd58bbb80826ecd323a830d29
jlstevens <jlstevens@ed.ac.uk>
2018-09-21 11:43:12 -0500

    Added unit test for unwatch

--- tests/API1/testwatch.py
+++ tests/API1/testwatch.py
@@ -37,6 +37,18 @@ class TestWatch(API1TestCase):
         obj.a = 1
         self.assertEqual(self.accumulator, 1)
 
+
+    def test_untriggered_when_unwatched(self):
+        def accumulator(change):
+            self.accumulator += change.new
+
+        obj = self.SimpleWatchExample()
+        obj.param.watch(accumulator, 'a')
+        obj.a = 1
+        self.assertEqual(self.accumulator, 1)
+        obj.param.unwatch(accumulator, 'a')
+        obj.a = 2
+        self.assertEqual(self.accumulator, 1)
 if __name__ == "__main__":
     import nose
     nose.runmodule()

c0b8244738a56b7e6cf455dd26566a7b3cc07da5
jlstevens <jlstevens@ed.ac.uk>
2018-09-21 11:45:53 -0500

    Added corresponding set up unit tests for watch_values

--- tests/API1/testwatch.py
+++ tests/API1/testwatch.py
@@ -49,6 +49,55 @@ class TestWatch(API1TestCase):
         obj.param.unwatch(accumulator, 'a')
         obj.a = 2
         self.assertEqual(self.accumulator, 1)
+
+
+
+class TestWatchValues(API1TestCase):
+
+    class SimpleWatchExample(param.Parameterized):
+        a = param.Integer(default=0)
+
+    def setUp(self):
+        super(TestWatchValues, self).setUp()
+        self.accumulator = 0
+
+    def test_triggered_when_values_changed(self):
+        def accumulator(a):
+            self.accumulator += a
+
+        obj = self.SimpleWatchExample()
+        obj.param.watch_values(accumulator, 'a')
+        obj.a = 1
+        self.assertEqual(self.accumulator, 1)
+        obj.a = 2
+        self.assertEqual(self.accumulator, 3)
+
+
+    def test_untriggered_when_values_unchanged(self):
+        def accumulator(a):
+            self.accumulator += a
+
+        obj = self.SimpleWatchExample()
+        obj.param.watch_values(accumulator, 'a')
+        obj.a = 1
+        self.assertEqual(self.accumulator, 1)
+        obj.a = 1
+        self.assertEqual(self.accumulator, 1)
+
+
+    def test_untriggered_when_values_unwatched(self):
+        def accumulator(a):
+            self.accumulator += a
+
+        obj = self.SimpleWatchExample()
+        obj.param.watch_values(accumulator, 'a')
+        obj.a = 1
+        self.assertEqual(self.accumulator, 1)
+        obj.param.unwatch_values(accumulator, 'a')
+        obj.a = 2
+        self.assertEqual(self.accumulator, 1)
+
+
 if __name__ == "__main__":
     import nose
     nose.runmodule()

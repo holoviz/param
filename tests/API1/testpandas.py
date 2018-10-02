27dfa45461b14a55df92b97014f8cc9830c44dc9
jlstevens <jlstevens@ed.ac.uk>
2018-10-02 14:06:04 -0500

    Added four unit tests of param.DataFrame

--- tests/API1/testpandas.py
+++ tests/API1/testpandas.py
@@ -16,16 +16,55 @@ except ImportError:
         raise unittest.SkipTest("pandas not available")
 
 
-class TestPandas(API1TestCase):
+class TestDataFrame(API1TestCase):
 
-    def test_simple_dataframe_param(self):
-        class Z(param.Parameterized):
-            z = param.DataFrame(default=pandas.DataFrame())
+    def test_empty_dataframe_param_invalid_set(self):
+        empty = pandas.DataFrame()
+        class Test(param.Parameterized):
+            df = param.DataFrame(default=empty)
 
-        z = Z()
-        exception = "Parameter 'z' value must be an instance of DataFrame, not '3'"
+        test = Test()
+        exception = "Parameter 'df' value must be an instance of DataFrame, not '3'"
         with self.assertRaisesRegexp(ValueError, exception):
-            z.z = 3
+            test.df = 3
+
+    def test_dataframe_unordered_column_set_valid(self):
+        valid = pandas.DataFrame({'a':[1,2], 'b':[2,3], 'c':[4,5]}, columns=['b', 'a'])
+        class Test(param.Parameterized):
+            df = param.DataFrame(default=valid, columns={'a', 'b'})
+
+
+    def test_dataframe_unordered_column_set_invalid(self):
+        valid_df = pandas.DataFrame({'a':[1,2], 'b':[2,3], 'd':[4,5]}, columns=['b', 'a', 'd'])
+        invalid_df = pandas.DataFrame({'a':[1,2], 'b':[2,3], 'c':[4,5]}, columns=['b', 'a', 'c'])
+
+        class Test(param.Parameterized):
+            df = param.DataFrame(default=valid_df, columns={'a', 'd'})
+
+
+        test = Test()
+        exception = "Provided DataFrame columns \['b', 'a', 'c'\] does not contain required columns \['a', 'd'\]"
+        with self.assertRaisesRegexp(Exception, exception):
+            test.df = invalid_df
+
+    def test_dataframe_ordered_column_list_valid(self):
+        valid = pandas.DataFrame({'a':[1,2], 'b':[2,3], 'c':[4,5]}, columns=['b', 'a', 'c'])
+        class Test(param.Parameterized):
+            test = param.DataFrame(default=valid, columns=['b', 'a', 'c'])
+
+
+    def test_dataframe_ordered_column_list_invalid(self):
+        valid_df = pandas.DataFrame({'a':[1,2], 'b':[2,3], 'd':[4,5]}, columns=['b', 'a', 'd'])
+        invalid_df = pandas.DataFrame({'a':[1,2], 'b':[2,3], 'c':[4,5]}, columns=['a', 'b', 'd'])
+
+        class Test(param.Parameterized):
+            df = param.DataFrame(default=valid_df, columns=['b', 'a', 'd'])
+
+
+        test = Test()
+        exception = "Provided DataFrame columns \['a', 'b', 'd'\] must exactly match \['b', 'a', 'd'\]"
+        with self.assertRaisesRegexp(Exception, exception):
+            test.df = invalid_df
 
 
 if __name__ == "__main__":

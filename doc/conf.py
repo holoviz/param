41d0b93cda31d6f9406671b950ee492cc6b41542
James A. Bednar <jbednar@continuum.io>
2018-10-12 01:59:34 -0500

    Moved gitter site

--- doc/conf.py
+++ doc/conf.py
@@ -33,7 +33,7 @@ html_context.update({
     'NAV': _NAV,
     'LINKS': _NAV,
     'SOCIAL': (
-        ('Gitter', '//gitter.im/ioam/holoviews'),
+        ('Gitter', '//gitter.im/pyviz/pyviz'),
         ('Github', '//github.com/ioam/param'),
     )
 })

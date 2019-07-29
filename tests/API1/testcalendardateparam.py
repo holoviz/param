a5246eec63e52508f9f774b461df77f86469b3f9
Gabriel Corona <gabriel.corona@enst-bretagne.fr>
2019-07-29 23:52:37 +0200

    Add Calendar and CalendarDateRange for real date types
    Date and DateRange as kepts for hybrid date/datetime for compatibility.

---
+++ tests/API1/testcalendardateparam.py
@@ -0,0 +1,54 @@
+"""
+Unit test for CalendarDate parameters.
+"""
+
+
+import datetime as dt
+import param
+from . import API1TestCase
+
+
+class TestDateTimeParameters(API1TestCase):
+
+    def test_initialization_out_of_bounds(self):
+        try:
+            class Q(param.Parameterized):
+                q = param.CalendarDate(dt.date(2017,2,27),
+                                       bounds=(dt.date(2017,2,1),
+                                               dt.date(2017,2,26)))
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("No exception raised on out-of-bounds date")
+
+    def test_set_out_of_bounds(self):
+        class Q(param.Parameterized):
+            q = param.CalendarDate(bounds=(dt.date(2017,2,1),
+                                           dt.date(2017,2,26)))
+        try:
+            Q.q = dt.date(2017,2,27)
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("No exception raised on out-of-bounds date")
+
+    def test_set_exclusive_out_of_bounds(self):
+        class Q(param.Parameterized):
+            q = param.CalendarDate(bounds=(dt.date(2017,2,1),
+                                           dt.date(2017,2,26)),
+                                   inclusive_bounds=(True, False))
+        try:
+            Q.q = dt.date(2017,2,26)
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("No exception raised on out-of-bounds date")
+
+    def test_get_soft_bounds(self):
+        q = param.CalendarDate(dt.date(2017,2,25),
+                               bounds=(dt.date(2017,2,1),
+                                       dt.date(2017,2,26)),
+                               softbounds=(dt.date(2017,2,1),
+                                           dt.date(2017,2,25)))
+        self.assertEqual(q.get_soft_bounds(), (dt.date(2017,2,1),
+                                               dt.date(2017,2,25)))
---
+++ tests/API1/testcalendardaterangeparam.py
@@ -0,0 +1,78 @@
+"""
+Unit tests for CalendarDateRange parameter.
+"""
+
+import datetime as dt
+import param
+from . import API1TestCase
+
+# Assuming tests of range parameter cover most of what's needed to
+# test date range.
+
+class TestDateTimeRange(API1TestCase):
+
+    bad_range = (dt.date(2017,2,27),dt.date(2017,2,26))
+
+    def test_wrong_type_default(self):
+        try:
+            class Q(param.Parameterized):
+                a = param.CalendarDateRange(default=(1.0,2.0))
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("Bad date type was accepted.")
+
+    def test_wrong_type_init(self):
+        class Q(param.Parameterized):
+            a = param.CalendarDateRange()
+
+        try:
+            Q(a=self.bad_range)
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("Bad date type was accepted.")
+
+    def test_wrong_type_set(self):
+        class Q(param.Parameterized):
+            a = param.CalendarDateRange()
+        q = Q()
+
+        try:
+            q.a = self.bad_range
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("Bad date type was accepted.")
+
+    def test_start_before_end_default(self):
+        try:
+            class Q(param.Parameterized):
+                a = param.CalendarDateRange(default=self.bad_range)
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("Bad date range was accepted.")
+
+    def test_start_before_end_init(self):
+        class Q(param.Parameterized):
+            a = param.CalendarDateRange()
+
+        try:
+            Q(a=self.bad_range)
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("Bad date range was accepted.")
+
+    def test_start_before_end_set(self):
+        class Q(param.Parameterized):
+            a = param.CalendarDateRange()
+
+        q = Q()
+        try:
+            q.a = self.bad_range
+        except ValueError:
+            pass
+        else:
+            raise AssertionError("Bad date range was accepted.")

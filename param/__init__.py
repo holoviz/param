fbbe2d9be713ac75cc67fc84bc9272dd3f8a1c4f
Philipp Rudiger <prudiger@anaconda.com>
2019-11-28 05:17:59 -0600

    Fix ClassSelector.get_range() for class_ tuple (#360)

--- param/__init__.py
+++ param/__init__.py
@@ -1292,7 +1292,7 @@ class Selector(ObjectSelector):
 
 class ClassSelector(SelectorBase):
     """
-    Parameter whose value is a specified class or an instance of that class.
+    Parameter allowing selection of either a subclass or an instance of a given set of classes.
     By default, requires an instance, but if is_instance=False, accepts a class instead.
     Both class and instance values respect the instantiate slot, though it matters only
     for is_instance=True.
@@ -1335,8 +1335,11 @@ class ClassSelector(SelectorBase):
         Only classes from modules that have been imported are added
         (see concrete_descendents()).
         """
-        classes = concrete_descendents(self.class_)
-        d=OrderedDict((name,class_) for name,class_ in classes.items())
+        classes = self.class_ if isinstance(self.class_, tuple) else (self.class_,)
+        all_classes = {}
+        for cls in classes:
+            all_classes.update(concrete_descendents(cls))
+        d=OrderedDict((name,class_) for name,class_ in all_classes.items())
         if self.allow_None:
             d['None']=None
         return d

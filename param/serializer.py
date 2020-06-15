c353f7b21a42619a98ddbbce272f676cf9fe5a78
jlstevens <jlrstevens@gmail.com>
2020-06-15 11:37:22 +0200

    Added support for ListSelector schema

--- param/serializer.py
+++ param/serializer.py
@@ -173,3 +173,16 @@ class JSONSerialization(Serialization):
                        'serialization due to unserializable type in objects')
                 raise UnsafeserializableException(msg)
             return {}
+
+    @classmethod
+    def listselector_schema(cls, p, safe=False):
+        if p.objects is None:
+            if safe is True:
+                msg = ('ListSelector cannot be guaranteed to be safe for '
+                       'serialization as allowed objects unspecified')
+            return {'type': 'array'}
+        for obj in p.objects:
+            if type(obj) not in cls.json_schema_literal_types:
+                msg = "ListSelector cannot serialize type %s" % type(obj)
+                raise UnserializableException(msg)
+        return {'type': 'array', 'items':{'enum':p.objects}}

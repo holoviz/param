44ac5860df6646f9805188267b33c80eca9a87db
James A. Bednar <jbednar@continuum.io>
2020-10-07 16:16:05 -0500

    Fixed test

--- param/serializer.py
+++ param/serializer.py
@@ -230,9 +230,9 @@ class JSONSerialization(Serialization):
     def objectselector_schema(cls, p, safe=False):
         try:
             allowed_types = [{'type': cls.json_schema_literal_types[type(obj)]}
-                             for obj in p.objects]
+                             for obj in p.objects.values()]
             schema =  { "anyOf": allowed_types}
-            schema['enum'] = p.objects
+            schema['enum'] = list(p.objects.values())
             return schema
         except:
             if safe is True:

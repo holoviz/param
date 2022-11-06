3f11ebebbedcf3a59bebf4c757412aa1b95c1fb3
maximlt <mliquet@anaconda.com>
2022-11-06 11:44:55 +0100

    fix flake

--- param/_async.py
+++ param/_async.py
@@ -5,21 +5,21 @@ to py2 incompatibility with both `async/await` and `yield from` syntax.
 """
 
 def generate_depends(func):
-    async def _depends(*args, **kw):
-        await func(*args, **kw)
+    async def _depends(*args, **kw):  # noqa: E999
+        await func(*args, **kw)  # noqa: E999
     return _depends
 
 def generate_caller(function, what='value', changed=None, callback=None, skip_event=None):
-    async def caller(*events):
+    async def caller(*events):  # noqa: E999
         if callback:
             callback(*events)
         if not skip_event or not skip_event(*events, what=what, changed=changed):
-            await function()
+            await function()  # noqa: E999
     return caller
 
 def generate_callback(func, dependencies, kw):
-    async def cb(*events):
+    async def cb(*events):  # noqa: E999
         args = (getattr(dep.owner, dep.name) for dep in dependencies)
         dep_kwargs = {n: getattr(dep.owner, dep.name) for n, dep in kw.items()}
-        await func(*args, **dep_kwargs)
+        await func(*args, **dep_kwargs)  # noqa: E999
     return cb

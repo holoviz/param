d31594376cc62a2cff9679b6f2e72539bfe66845
maximlt <mliquet@anaconda.com>
2023-03-12 14:40:17 +0100

    use coverage directly instead of pytest-cov

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ extras_require = {
     # (https://github.com/pypa/pip/issues/1197)
     'tests': [
         'pytest',
-        'pytest-cov',
+        'coverage',
         'flake8',
     ],
     'doc': [

9bfe1576787d66d631464ef76f24a3cca2a3c5f9
maximlt <mliquet@anaconda.com>
2023-03-14 17:03:18 +0100

    soft removal of Python 2.7 and Python 3.6

--- setup.py
+++ setup.py
@@ -75,10 +75,7 @@ setup_args = dict(
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",

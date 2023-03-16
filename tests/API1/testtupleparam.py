1c9539768f06253a08992f3d45fa174722d8f80b
maximlt <mliquet@anaconda.com>
2023-03-16 13:30:54 +0100

    add a test to Tuple

--- tests/API1/testtupleparam.py
+++ tests/API1/testtupleparam.py
@@ -162,6 +162,22 @@ class TestTupleParameters(API1TestCase):
 
         assert b.param.p.length == 2
 
+    def test_inheritance_length_behavior3(self):
+        class A(param.Parameterized):
+            p = param.Tuple(default=(0, 1, 2))
+
+        class B(A):
+            p = param.Tuple(default=(0, 1, 2, 3))
+
+        assert B.p == (0, 1, 2, 3)
+        assert B.param.p.length == 4
+
+        b = B()
+
+        assert b.p == (0, 1, 2, 3)
+        assert b.param.p.default == (0, 1, 2, 3)
+        assert b.param.p.length == 4
+
 
 class TestNumericTupleParameters(API1TestCase):
 

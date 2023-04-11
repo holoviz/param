9e3f2a981817a46b996c83998463bb71ed915e2b
maximlt <mliquet@anaconda.com>
2023-04-11 18:42:40 +0200

    add docstring to _compute_selector_default

--- param/__init__.py
+++ param/__init__.py
@@ -1222,6 +1222,12 @@ class SelectorBase(Parameter):
 
 
 def _compute_selector_default(p):
+    """
+    Using a function instead of setting default to [] in _slot_defaults, as
+    if it were modified in place later, which would happen with check_on_set set to False,
+    then the object in _slot_defaults would itself be updated and the next Selector
+    instance created wouldn't have [] as the default but a populated list.
+    """
     return []
 
 

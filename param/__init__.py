15e819c3dd2be2ec235dac1b047f46c633aaad11
maximlt <mliquet@anaconda.com>
2023-04-29 14:19:57 +0200

    more changes

--- param/__init__.py
+++ param/__init__.py
@@ -40,15 +40,23 @@ from .parameterized import shared_parameters # noqa: api import
 from .parameterized import logging_level     # noqa: api import
 from .parameterized import DEBUG, VERBOSE, INFO, WARNING, ERROR, CRITICAL # noqa: api import
 
-
-# Determine up-to-date version information, if possible, but with a
-# safe fallback to ensure that this file and parameterized.py are the
-# only two required files.
+# Define '__version__'
 try:
-    from .version import Version
-    __version__ = str(Version(fpath=__file__, archive_commit="$Format:%h$", reponame="param"))
-except:
-    __version__ = "0.0.0+unknown"
+    # If setuptools_scm is installed (e.g. in a development environment with
+    # an editable install), then use it to determine the version dynamically.
+    from setuptools_scm import get_version
+
+    # This will fail with LookupError if the package is not installed in
+    # editable mode or if Git is not installed.
+    __version__ = get_version(root="..", relative_to=__file__)
+except (ImportError, LookupError):
+    # As a fallback, use the version that is hard-coded in the file.
+    try:
+        from ._version import __version__
+    except ModuleNotFoundError:
+        # The user is probably trying to run this without having installed
+        # the package.
+        __version__ = "0.0.0+unknown"
 
 try:
     import collections.abc as collections_abc

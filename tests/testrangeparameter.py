78bbf5cae5fb88df14322d80464c4a67bf020f68
maximlt <mliquet@anaconda.com>
2023-08-28 10:56:44 +0200

    update tests to rely on pytest

--- tests/testrangeparameter.py
+++ tests/testrangeparameter.py
@@ -133,32 +133,29 @@ class TestRangeParameters(unittest.TestCase):
     def test_set_exclusive_out_of_bounds_upper(self):
         class Q(param.Parameterized):
             q = param.Range(bounds=(0, 10), inclusive_bounds=(True, False))
-        try:
+        with pytest.raises(
+            ValueError,
+            match=re.escape("Range parameter 'Q.q' upper bound must be in range [0, 10), not 10.")
+        ):
             Q.q = (0, 10)
-        except ValueError:
-            pass
-        else:
-            raise AssertionError("No exception raised on out-of-bounds date")
 
     def test_set_exclusive_out_of_bounds_lower(self):
         class Q(param.Parameterized):
             q = param.Range(bounds=(0, 10), inclusive_bounds=(False, True))
-        try:
+        with pytest.raises(
+            ValueError,
+            match=re.escape("Range parameter 'Q.q' lower bound must be in range (0, 10], not 0.")
+        ):
             Q.q = (0, 10)
-        except ValueError:
-            pass
-        else:
-            raise AssertionError("No exception raised on out-of-bounds date")
 
     def test_set_out_of_bounds(self):
         class Q(param.Parameterized):
             q = param.Range(bounds=(0, 10))
-        try:
+        with pytest.raises(
+            ValueError,
+            match=re.escape("Range parameter 'Q.q' upper bound must be in range [0, 10], not 11.")
+        ):
             Q.q = (5, 11)
-        except ValueError:
-            pass
-        else:
-            raise AssertionError("No exception raised on out-of-bounds date")
 
     def test_get_soft_bounds(self):
         q = param.Range((1,3), bounds=(0, 10), softbounds=(1, 9))

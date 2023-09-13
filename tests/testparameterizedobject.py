dada602d9ec4e899d48e0a2238c61d3e176e0f84
maximlt <mliquet@anaconda.com>
2023-09-13 14:47:13 +0200

    add more tests

--- tests/testparameterizedobject.py
+++ tests/testparameterizedobject.py
@@ -1347,37 +1347,113 @@ def test_inheritance_class_attribute_behavior():
     # https://github.com/holoviz/param/issues/718
     assert B.p == 1
 
+class TestShallowCopyMutableAttributes:
 
-class CustomParameter(Parameter):
+    @pytest.fixture
+    def foo(self):
+        class Foo:
+            def __init__(self, val):
+                self.val = val
 
-    __slots__ = ['container']
+        return Foo
 
-    _slot_defaults = _dict_update(Parameter._slot_defaults, container=None)
+    @pytest.fixture
+    def custom_param(self):
+        class CustomParameter(Parameter):
 
-    def __init__(self, default=Undefined, *, container=Undefined, **kwargs):
-        super().__init__(default=default, **kwargs)
-        self.container = container
+            __slots__ = ['container']
+
+            _slot_defaults = _dict_update(Parameter._slot_defaults, container=None)
+
+            def __init__(self, default=Undefined, *, container=Undefined, **kwargs):
+                super().__init__(default=default, **kwargs)
+                self.container = container
+
+        return CustomParameter
+
+    def test_shallow_copy_on_class_creation(self, custom_param, foo):
+        clist = [foo(1), foo(2)]
+
+        class P(param.Parameterized):
+            cp = custom_param(container=clist)
 
 
-def test_inheritance_container_slot_shallow_copied():
+        # the mutable container has been shallow-copied
+        assert P.param.cp.container is not clist
+        assert all(cval is val for cval, val in zip(P.param.cp.container, clist))
 
-    clist = [1, 2]
+    def test_shallow_copy_inheritance_each_level(self, custom_param):
 
-    class A(param.Parameterized):
-        p = CustomParameter(container=clist)
+        clist = [1, 2]
 
-    class B(A):
-        p = CustomParameter(default=1)
+        class A(param.Parameterized):
+            p = custom_param(container=clist)
 
-    clist.append(3)
+        class B(A):
+            p = custom_param(default=1)
 
-    assert A.param.p.container == [1, 2]
-    assert B.param.p.container == [1, 2]
+        clist.append(3)
 
-    B.param.p.container.append(4)
+        assert A.param.p.container == [1, 2]
+        assert B.param.p.container == [1, 2]
 
-    assert A.param.p.container == [1, 2]
-    assert B.param.p.container == [1, 2, 4]
+        B.param.p.container.append(4)
+
+        assert A.param.p.container == [1, 2]
+        assert B.param.p.container == [1, 2, 4]
+
+    def test_shallow_copy_on_instance_getitem(self, custom_param, foo):
+        clist = [foo(1), foo(2)]
+
+        class P(param.Parameterized):
+            cp = custom_param(container=clist)
+
+        p = P()
+
+        assert 'cp' not in p._param__private.params
+
+        p.param['cp']
+
+        # the mutable container has been shallow-copied
+        assert 'cp' in p._param__private.params
+        assert P.param.cp.container == p._param__private.params['cp'].container
+        assert P.param.cp.container is not p._param__private.params['cp'].container
+        assert all(cval is val for cval, val in zip(p.param.cp.container, clist))
+
+    def test_shallow_copy_on_instance_set(self, custom_param, foo):
+        clist = [foo(1), foo(2)]
+
+        class P(param.Parameterized):
+            cp = custom_param(container=clist)
+
+        p = P()
+
+        assert 'cp' not in p._param__private.params
+
+        p.cp = 'value'
+
+        # the mutable container has been shallow-copied
+        assert 'cp' in p._param__private.params
+        assert P.param.cp.container == p._param__private.params['cp'].container
+        assert P.param.cp.container is not p._param__private.params['cp'].container
+        assert all(cval is val for cval, val in zip(p.param.cp.container, clist))
+
+    def test_modify_class_container_before_shallow_copy(self, custom_param, foo):
+        clist = [foo(1), foo(2)]
+        clist2 = [foo(3), foo(4)]
+
+        class P(param.Parameterized):
+            cp = custom_param(container=clist)
+
+        p1 = P()
+        p2 = P()
+
+        # Setting the class container will affect instances are the shallow copy
+        # is lazy and has not yet been made.
+        P.param.cp.container = clist2
+
+        assert p1.param.cp.container == clist2
+        assert p2.param.cp.container == clist2
 
 
 @pytest.fixture

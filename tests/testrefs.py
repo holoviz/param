90831e2dd1f2f626ee97ad20747afee3b2b3c6f6
Philipp Rudiger <prudiger@anaconda.com>
2024-02-12 12:19:08 +0100

    Add larger timeout

--- tests/testrefs.py
+++ tests/testrefs.py
@@ -169,7 +169,7 @@ async def test_generator_ref():
 
     await asyncio.sleep(0.02)
     assert p.string == 'string?'
-    await asyncio.sleep(0.05)
+    await asyncio.sleep(0.1)
     assert p.string == 'string!'
 
 async def test_async_generator_ref_cancelled():

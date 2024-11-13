edda46b87429e98d67c346d190d0b32ab73e602c
Simon Høxbro Hansen <simon.hansen@me.com>
2024-11-13 21:29:54 +0100

    Fix lint

--- tests/testreactive.py
+++ tests/testreactive.py
@@ -770,6 +770,6 @@ def test_reactive_callback_resolve_accessor():
     assert out is df["name"].str
 
 def test_docstrings_in_sync():
-    # The docstring needs to be explicitly written to work with LSP. 
+    # The docstring needs to be explicitly written to work with LSP.
     assert dedent(reactive_ops.__doc__) == dedent(Parameter.rx.__doc__)
     assert dedent(reactive_ops.__doc__) == dedent(rx.rx.__doc__)

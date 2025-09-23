bbaea5b8a64452d346afa05f6b18ba9cdd41f5a7
Maxime Liquet <35924738+maximlt@users.noreply.github.com>
2025-09-23 20:19:03 +0200

    Deprecate setting a parameter value before full instance initialization (#1025)

--- param/parameterized.py
+++ param/parameterized.py
@@ -45,6 +45,7 @@ from . import serializer
 from ._utils import (
     DEFAULT_SIGNATURE,
     ParamFutureWarning as _ParamFutureWarning,
+    ParamPendingDeprecationWarning as _ParamPendingDeprecationWarning,
     Skip,
     _deprecated,
     _deprecate_positional_args,
@@ -1880,6 +1881,15 @@ class Parameter(_ParameterBase):
             else:
                 # When setting a Parameter before calling super.
                 if not isinstance(obj._param__private, _InstancePrivate):
+                    warnings.warn(
+                        f"Setting the Parameter {self.name!r} to {val!r} before "
+                        f"the Parameterized class {type(obj).__name__!r} is fully "
+                        "instantiated is deprecated and will raise an error in "
+                        "a future version. Ensure the value is set after calling "
+                        "`super().__init__(**params)` in the constructor.",
+                        category=_ParamPendingDeprecationWarning,
+                        stacklevel=4,
+                    )
                     obj._param__private = _InstancePrivate(
                         explicit_no_refs=type(obj)._param__private.explicit_no_refs
                     )

26c604683f9987a4fc89ab3b4bedb8259cededb5
maximlt <mliquet@anaconda.com>
2025-10-24 16:49:57 +0200

    update a test to pass in setup-dev moed

--- tests/testimports.py
+++ tests/testimports.py
@@ -4,12 +4,21 @@ from textwrap import dedent
 
 
 def test_no_blocklist_imports():
-    check = """\
+    # setuptools_scm is imported when importing param. It itself imports
+    # the logging module.
+    blocklist = {"numpy", "IPython", "pandas", "asyncio", "html", "logging"}
+    try:
+        import setuptools_scm  # noqa
+        blocklist.remove("logging")
+    except ModuleNotFoundError:
+        pass
+
+    check = f"""\
     import sys
     sys.modules.pop("logging", None)  # Added because of coverage will add it
     import param
 
-    blocklist = {"numpy", "IPython", "pandas", "asyncio", "html", "logging"}
+    blocklist = {blocklist}
     mods = blocklist & set(sys.modules)
 
     if mods:

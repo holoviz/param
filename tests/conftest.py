361d87b05a9533446a02400b1ec1212e976cab17
maximlt <mliquet@anaconda.com>
2025-10-28 16:33:49 +0100

    without pandas

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ param.parameterized.warnings_as_exceptions = True
 
 @pytest.fixture
 def dataframe():
-    import pandas as pd
+    pd = pytest.importorskip("pandas")
     return pd.DataFrame({
         'int': [1, 2, 3],
         'float': [3.14, 6.28, 9.42],

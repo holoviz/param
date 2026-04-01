062985790bdca1eccfd587abb1ddc498853f5ee3
Philipp Rudiger <prudiger@anaconda.com>
2026-04-01 11:24:11 +0200

    Fix up import test

--- tests/testimports.py
+++ tests/testimports.py
@@ -6,16 +6,10 @@ from textwrap import dedent
 def test_no_blocklist_imports():
     # setuptools_scm is imported when importing param. It itself imports
     # the logging module.
-    blocklist = {"numpy", "IPython", "pandas", "asyncio", "html", "logging"}
-    try:
-        import setuptools_scm  # noqa
-        blocklist.remove("logging")
-    except ModuleNotFoundError:
-        pass
+    blocklist = {"numpy", "IPython", "pandas", "asyncio", "html"}
 
     check = f"""\
     import sys
-    sys.modules.pop("logging", None)  # Added because of coverage will add it
     import param
 
     blocklist = {blocklist}
